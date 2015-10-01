44810b64a4cc3bc1a668f0b348e850279eae45af
Meffody <meffody@bk.ru>
2015-10-01 21:49:15 +0300

    Lessons 1 + Home 1 = Done!

---
+++ JavaSEAddressBook/src/com/example/tests/ContactsCreationTests.java
@@ -0,0 +1,52 @@
+package com.example.tests;
+
+import org.testng.annotations.Test;
+
+public class ContactsCreationTests extends TestBase {
+	@Test
+	public void testNonEmptyContactsCreation() throws Exception {
+		openMainPage();
+		goToAddNewContactsPage();
+		ContactsDate contact = new ContactsDate();
+		contact.firstName = "FirstName";
+		contact.lastName = "LastName";
+		contact.address = "Address";
+		contact.home = "Home";
+		contact.mobile = "Mobile";
+		contact.work = "Work";
+		contact.email = "Email";
+		contact.emailTwo = "Email2";
+		contact.birthDay = "1";
+		contact.birthMonth = "January";
+		contact.birthYear = "1992";
+		contact.secondaryAddress = "SecondaryAddress";
+		contact.secondaryHome = "SecondaryHome";
+		fillContactsForm(contact);
+		submitContactsCreation();
+		openMainPage();
+	}
+	
+	@Test
+	public void testNonEmptyContactsCreation2() throws Exception {
+		openMainPage();
+		goToAddNewContactsPage();
+		ContactsDate contact = new ContactsDate();
+		contact.firstName = "FirstName2";
+		contact.lastName = "LastName2";
+		contact.address = "Address2";
+		contact.home = "Home2";
+		contact.mobile = "Mobile2";
+		contact.work = "Work2";
+		contact.email = "Email2";
+		contact.emailTwo = "Email22";
+		contact.birthDay = "2";
+		contact.birthMonth = "January";
+		contact.birthYear = "1993";
+		contact.secondaryAddress = "SecondaryAddress2";
+		contact.secondaryHome = "SecondaryHome2";
+		fillContactsForm(contact);
+		submitContactsCreation();
+		openMainPage();
+	}
+
+}
---
+++ JavaSEAddressBook/src/com/example/tests/ContactsDate.java
@@ -0,0 +1,38 @@
+package com.example.tests;
+
+public class ContactsDate {
+	public String firstName;
+	public String lastName;
+	public String address;
+	public String home;
+	public String mobile;
+	public String work;
+	public String email;
+	public String emailTwo;
+	public String birthDay;
+	public String birthMonth;
+	public String birthYear;
+	public String secondaryAddress;
+	public String secondaryHome;
+
+	public ContactsDate() {
+
+	}
+
+	public ContactsDate(String firstName, String lastName, String address, String home, String mobile, String work,
+			String email, String emailTwo, String birthDay, String birthMonth, String birthYear, String secondaryAddress, String secondaryHome) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.home = home;
+		this.mobile = mobile;
+		this.work = work;
+		this.email = email;
+		this.emailTwo = emailTwo;
+		this.birthDay = birthDay;
+		this.birthMonth = birthMonth;
+		this.birthYear = birthYear;
+		this.secondaryAddress = secondaryAddress;
+		this.secondaryHome = secondaryHome;
+	}
+}
